b28d38de1cd25ccb8b0e3f63b45f3b0f070b7827
Ziyan Wang <wangziyan37@gmail.com>
2024-05-05 00:16:25 -0400

    Update setup_experiment.py

--- learned_optimization/setup_experiment.py
+++ learned_optimization/setup_experiment.py
@@ -20,7 +20,6 @@ import os
 from typing import Optional
 
 from absl import flags
-from absl import logging
 import gin
 import jax
 from learned_optimization import filesystem
@@ -55,7 +54,6 @@ def parse_and_set_gin_config(finalize: bool, skip_unknown: bool):
   # strings.
   if FLAGS.gin_import:
     for imp in FLAGS.gin_import:
-      logging.info("Gin is importing %s", imp)
       # glob imports
       if "*" in imp:
         # Figure out which folder the module is in, then import all modules
@@ -66,7 +64,6 @@ def parse_and_set_gin_config(finalize: bool, skip_unknown: bool):
         for p in glob.glob(os.path.join(os.path.dirname(path), "*.py")):
           p = p.split("/")[-1].replace(".py", "")
           to_import = prefix + "." + p
-          logging.info("Gin is importing %s from glob", to_import)
           __import__(to_import)
       else:
         __import__(imp)
@@ -124,10 +121,10 @@ def setup_experiment(
     filesystem.make_dirs(FLAGS.train_log_dir)
 
   if FLAGS.train_log_dir:
-    logging.info(
-        "Setup experiment! Training directory located: %s", FLAGS.train_log_dir
-    )
+    # logging.info(
+    #     "Setup experiment! Training directory located: %s", FLAGS.train_log_dir
+    # )
     return FLAGS.train_log_dir
   else:
-    logging.info("Setup experiment! No training directory specified")
+    # logging.info("Setup experiment! No training directory specified")
     return None
